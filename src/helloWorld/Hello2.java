ec891359d79fbc2809f63a78f80ad93a64f2d3dc
Omar Navarro <jomarnavarro@gmail.com>
2019-01-25 21:44:55 -0600

    Creates javaProcedural examples.

---
+++ src/helloWorld/Hello2.java
@@ -0,0 +1,13 @@
+package helloWorld;
+
+import libs.Input;
+
+public class Hello2 {
+
+	public static void main(String args[])
+	{
+		System.out.print("Cual es tu nombre?: ");
+		String name = Input.get_string();
+		System.out.println("hello, "+ name + "\n");   
+	}
+}
