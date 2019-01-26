700dce120e6b16b5652995282616cce9d04bf591
Omar Navarro <jomarnavarro@gmail.com>
2019-01-26 10:25:02 -0600

    Agrega instrucciones al archivo EjemploFloat.java

--- src/numericTypes/EjemploFloat.java
+++ src/numericTypes/EjemploFloat.java
@@ -5,8 +5,9 @@ import libs.Input;
 public class EjemploFloat {
 
 	public static void main(String[] args) {
+		System.out.print("Ingresa un numero decimal ");
 		float f = Input.get_float();
-		
+		System.out.print("Ingresa otro numero decimal ");
 		float f2 = Input.get_float();
 		
 		Input.print("f/f2 = " + (f/f2));
